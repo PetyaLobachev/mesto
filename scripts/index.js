738bbc310cc2a67fa23aa9b3c3d83c0a05a568fb
Petr Lobachev <petya.lobachev@yandex.ru>
2020-09-02 22:14:04 +0300

    feat: добавить адаптив для мобильных устройств

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,34 @@
-// Edit Button - кнопка вызова всплывающего окна
-const profileEditButton = document.querySelector('.profile__edit-button')
-//Popup - всплывающее окно
-const popup = document.querySelector('.popup')
-const popupCloseButton = popup.querySelector('.popup__close-button')
-
-
-//Функция - для открывания popup
-const popupToggle = function () {
-  popup.classList.toggle('popup_opened')
+let body = document.querySelector(".body");
+//profileEditButton - кнопка редактирования профиля
+let profileEditButton = document.querySelector(".profile__edit-button");
+//popup - всплывающее окно
+let popup = document.querySelector(".popup");
+//popupCloseButton - кнопка закрытия редактирования профиля
+let popupCloseButton = popup.querySelector(".popup__close-button");
+//Функция - для открывания popup (всплывающее окно)
+function popupToggle() {
+  popup.classList.toggle("popup_opened");
+  body.classList.toggle("body_if-popup-opened");
 }
 //Подключаем слушатель для переменной profileEditButton (кнопка с классом .profile__edit-button)
-profileEditButton.addEventListener('click', popupToggle)
+profileEditButton.addEventListener("click", popupToggle);
+
 //Подключаем слушатель для переменной popupCloseButton (кнопка с классом .popup__close-button)
-popupCloseButton.addEventListener('click', popupToggle)
-
-
-
-
-
-
-const popupForm = popup.querySelector('.popup__form')
-const inputSubmitButton = popup.querySelector('.popup__submit-button')
-const nameAuthor = popupForm.querySelector('.popup__form-name-author')
-const aboutAuthor = popupForm.querySelector('.popup__form-about-author')
-const profileTitle = document.querySelector('.profile__title')
-const profileAbout = document.querySelector('.profile__about')
-
-const formSubmitHandler = function (evt) {
-  evt.preventDefault()
-  profileTitle.textContent = nameAuthor.value 
-  profileAbout.textContent = profileAbout.value 
+popupCloseButton.addEventListener("click", popupToggle);
+// Создание одноименных переменных для классов элементов
+let popupForm = popup.querySelector(".popup__form");
+let inputSubmitButton = popup.querySelector(".popup__submit-button");
+let nameAuthor = popupForm.querySelector(".popup__form-name-author");
+let aboutAuthor = popupForm.querySelector(".popup__form-about-author");
+let profileTitle = document.querySelector(".profile__title");
+let profileAboutAuthor = document.querySelector(".profile__about-author");
+//Функция вывода данных через input в профиль автора
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = nameAuthor.value;
+  profileAboutAuthor.textContent = aboutAuthor.value;
 }
-  popupForm.addEventListener('submit', formSubmitHandler)
+//Подключаем слушатель для переменной popupForm (со значаниями: отправить(submit), и функцией вывода данных: formSubmitHandler)
+popupForm.addEventListener("submit", formSubmitHandler);
+//Подключаем слушатель для переменной inputSubmitButton (кнопка с классом .popup__submit-button), для закрывания pop up (всплывающее окно) при отправке данных через кнопку "сохранить"
+inputSubmitButton.addEventListener("click", popupToggle);
