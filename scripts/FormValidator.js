0777c3ac0daea618302702c3de2ffc206f8f36cf
Petr <petya.lobachev@yandex.ru>
2020-10-13 19:36:17 +0300

    fix: bug

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -2,77 +2,84 @@
 //Класс Валидации форм
 class FormValidator {
   constructor(parametrs, formSelector) {
-  this._formElement = document.querySelector(formSelector);
-  this._formSelector = formSelector;
-  this._inputSelector = parametrs.inputSelector;
-  this._submitButtonSelector = parametrs.submitButtonSelector;
-  this._inactiveButtonClass = parametrs.inactiveButtonClass;
-  this._inputErrorClass = parametrs.inputErrorClass;
-  this._errorClass = parametrs.errorClass;
+    this._formElement = document.querySelector(formSelector);
+    this._formSelector = formSelector;
+    this._inputSelector = parametrs.inputSelector;
+    this._submitButtonSelector = parametrs.submitButtonSelector;
+    this._inactiveButtonClass = parametrs.inactiveButtonClass;
+    this._inputErrorClass = parametrs.inputErrorClass;
+    this._errorClass = parametrs.errorClass;
   }
   //Приватный метод показывает ошибку в поле ввода(input)
-_showInputError(popupInput, errorMessege) {
-  const errorElement = this._formElement.querySelector(`#${popupInput.id}-error`);
-  popupInput.classList.add(this._inputErrorClass);
-  errorElement.textContent = errorMessege;
-  errorElement.classList.add(this._errorClass);
-}
-//Приватный метод скрывает ошибку в поле ввода(input)
-_hideInputError(popupInput) {
-  const errorElement = this._formElement.querySelector(`#${popupInput.id}-error`);
-  popupInput.classList.remove(this._inputErrorClass);
-  errorElement.classList.remove(this._errorClass);
-  errorElement.textContent = "";
-}
-//Приватный метод проверяет массив полей ввода на предмет корректности введеных данных и 
-// в зависимости от этого вызывает методы показать или скрыть ошибку
-_checkInputValidity(popupInput) {
-  if (!popupInput.validity.valid) {
-    this._showInputError(popupInput, popupInput.validationMessage);
-  } else {
-    this._hideInputError(popupInput);
+  _showInputError(popupInput, errorMessege) {
+    const errorElement = this._formElement.querySelector(
+      `#${popupInput.id}-error`
+    );
+    popupInput.classList.add(this._inputErrorClass);
+    errorElement.textContent = errorMessege;
+    errorElement.classList.add(this._errorClass);
   }
-}
-//Приватный метод проверяет корректность полей ввода для кнопки submit
-_hasInvalidInput(inputList) {
-  return inputList.some(function(popupInput) {
-    return !popupInput.validity.valid;
-  });
-}
-//Приватный метод стилизации кнопок submit
-_toggleButtonState(inputList, inputSubmitButton) {
-  if (this._hasInvalidInput(inputList)) {
-    inputSubmitButton.classList.add(this._inactiveButtonClass);
-    inputSubmitButton.setAttribute("disabled", true);
-  } else {
-    inputSubmitButton.classList.remove(this._inactiveButtonClass);
-    inputSubmitButton.removeAttribute("disabled");
+  //Приватный метод скрывает ошибку в поле ввода(input)
+  _hideInputError(popupInput) {
+    const errorElement = this._formElement.querySelector(
+      `#${popupInput.id}-error`
+    );
+    popupInput.classList.remove(this._inputErrorClass);
+    errorElement.classList.remove(this._errorClass);
+    errorElement.textContent = "";
   }
-}
-//Приватный метод проходит по массиву input^ов => устанавливает слушителя событий полю ввода, 
-// в функции колбека вызывает методы 
-// 1. _checkInputValidity - проверки валиднойсти input^ов,
-// 2. _toggleButtonState - переключение состояния кнопки.
-_setEventListeners() {
-  const inputList = Array.from(this._formElement.querySelectorAll(this._inputSelector));
-  const inputSubmitButton = this._formElement.querySelector(this._submitButtonSelector);
-  this._toggleButtonState(inputList, inputSubmitButton);
-  inputList.forEach((popupInput) => {
-    popupInput.addEventListener("input", () => {
-      this._checkInputValidity(popupInput);
-      this._toggleButtonState(inputList, inputSubmitButton);
+  //Приватный метод проверяет массив полей ввода на предмет корректности введеных данных и
+  // в зависимости от этого вызывает методы показать или скрыть ошибку
+  _checkInputValidity(popupInput) {
+    if (!popupInput.validity.valid) {
+      this._showInputError(popupInput, popupInput.validationMessage);
+    } else {
+      this._hideInputError(popupInput);
+    }
+  }
+  //Приватный метод проверяет корректность полей ввода для кнопки submit
+  _hasInvalidInput(inputList) {
+    return inputList.some(function (popupInput) {
+      return !popupInput.validity.valid;
     });
-  });
-  
-}
-//Публичный метод включения валидации формы
-enableValidation (){
-  function submitFormHandler(event) {
-  event.preventDefault();
   }
-  this._formElement.addEventListener("submit", submitFormHandler)
-  this._setEventListeners();
-}
+  //Приватный метод стилизации кнопок submit
+  _toggleButtonState(inputList, inputSubmitButton) {
+    if (this._hasInvalidInput(inputList)) {
+      inputSubmitButton.classList.add(this._inactiveButtonClass);
+      inputSubmitButton.setAttribute("disabled", true);
+    } else {
+      inputSubmitButton.classList.remove(this._inactiveButtonClass);
+      inputSubmitButton.removeAttribute("disabled");
+    }
+  }
+  //Приватный метод проходит по массиву input^ов => устанавливает слушителя событий полю ввода,
+  // в функции колбека вызывает методы
+  // 1. _checkInputValidity - проверки валиднойсти input^ов,
+  // 2. _toggleButtonState - переключение состояния кнопки.
+  _setEventListeners() {
+    const inputList = Array.from(
+      this._formElement.querySelectorAll(this._inputSelector)
+    );
+    const inputSubmitButton = this._formElement.querySelector(
+      this._submitButtonSelector
+    );
+    this._toggleButtonState(inputList, inputSubmitButton);
+    inputList.forEach((popupInput) => {
+      popupInput.addEventListener("input", () => {
+        this._checkInputValidity(popupInput);
+        this._toggleButtonState(inputList, inputSubmitButton);
+      });
+    });
+  }
+  //Публичный метод включения валидации формы
+  enableValidation() {
+    function submitFormHandler(event) {
+      event.preventDefault();
+    }
+    this._formElement.addEventListener("submit", submitFormHandler);
+    this._setEventListeners();
+  }
 }
 
-export {FormValidator}
+export { FormValidator };
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
-import {Card} from './Card.js'
-import {FormValidator} from './FormValidator.js'
+import { Card } from "./Card.js";
+import { FormValidator } from "./FormValidator.js";
 
 const profileEditButton = document.querySelector(".profile__edit-button");
 const popup = document.querySelector(".popup");
@@ -15,14 +15,20 @@ const popupFormAddCard = popupAddCard.querySelector(".popup__form_add-card");
 const addButton = document.querySelector(".profile__add-button");
 const popupFormCardName = popupAddCard.querySelector(".popup__form_card_name");
 const popupFormCardLink = popupAddCard.querySelector(".popup__form_card_link");
-const popupCloseBattonAddCard = popupAddCard.querySelector(".popup__close-button_add-card");
+const popupCloseBattonAddCard = popupAddCard.querySelector(
+  ".popup__close-button_add-card"
+);
 const popupOpenCardImage = document.querySelector(".popup__open-card-image");
-const popupOpenCardFigcaption = document.querySelector(".popup__open-card-figcaption");
+const popupOpenCardFigcaption = document.querySelector(
+  ".popup__open-card-figcaption"
+);
 const popupOpenCard = document.querySelector(".popup__open-card");
-const popupCloseOpenCard = document.querySelector(".popup__close-button_open-card");
+const popupCloseOpenCard = document.querySelector(
+  ".popup__close-button_open-card"
+);
 
 //Клавиши клавиатуры
-const esc = 'Escape';
+const esc = "Escape";
 //Массив карточек, добавляется при загрузке страницы
 const initialCards = [
   {
@@ -63,22 +69,24 @@ const card = {
 };
 //Объект с классами селекторов для валидации форм
 const parametrs = {
-  formSelectorAddCard: '.popup__form_add-card',
+  formSelectorAddCard: ".popup__form_add-card",
   formSelector: ".popup__form",
   inputSelector: ".popup__input",
   submitButtonSelector: ".popup__submit-button",
   inactiveButtonClass: "popup__button_disabled",
   inputErrorClass: "popup__input_type_error",
-  errorClass: "popup__form-input-error_active"
+  errorClass: "popup__form-input-error_active",
 };
 //*-------------------------------Функции открытия и закрытия попапов-------------------------------*/
 //Функция открытия попапов
 function openPopup(evt) {
   evt.classList.add("popup_opened");
+  document.addEventListener("keydown", closePopupOnEsc);
 }
 //Функция закрытия попапов
 function closePopup(evt) {
   evt.classList.remove("popup_opened");
+  document.removeEventListener("keydown", closePopupOnEsc);
 }
 //Функция закрытия попапа по клику кнопки Escape
 function closePopupOnEsc(event) {
@@ -88,9 +96,6 @@ function closePopupOnEsc(event) {
     popupClosedAddCardHandler();
   }
 }
-//Подключение слушателя к документу для активации действия кнопки Escape
-document.addEventListener("keydown", closePopupOnEsc);
-
 //Функция закрытия попапа по клику на область затеменения(overlay) вокруг попап-контейнера
 function closePopapOnOverlay(event) {
   if (event.target !== event.currentTarget) {
@@ -139,8 +144,7 @@ popupForm.addEventListener("submit", formSubmitProfileHandler);
 //Функция добавления новой карточки
 function formSubmitHandlerAddCard(evt) {
   evt.preventDefault();
-  (card.name = popupFormCardName.value),
-  (card.link = popupFormCardLink.value);
+  (card.name = popupFormCardName.value), (card.link = popupFormCardLink.value);
   addCard();
   openPopup(popupAddCard);
   resetForm();
@@ -148,7 +152,7 @@ function formSubmitHandlerAddCard(evt) {
 }
 // Функция добавления новой карточки в начало страницы используя класс Card
 function addCard() {
-  const addNewCard = new Card(card, '.template')
+  const addNewCard = new Card(card, ".template");
   const newCard = addNewCard.createCard();
   cards.prepend(newCard);
 }
@@ -185,17 +189,23 @@ function popupClosedOpenCardHandler() {
   closePopup(popupOpenCard);
 }
 //Перебор массива объектов => создание карточек из класса Card и вывод их на страницу
-initialCards.forEach(function(card) {
-  const itemCard = new Card(card, '.template');
+initialCards.forEach(function (card) {
+  const itemCard = new Card(card, ".template");
   const cardElement = itemCard.createCard();
-  cards.append(cardElement)
-})
-//Создание нового объекта - экземляра класса для валидации формы EditProfile 
-const formValidatorEditProfile = new FormValidator(parametrs, parametrs.formSelector)
-formValidatorEditProfile.enableValidation()
+  cards.append(cardElement);
+});
+//Создание нового объекта - экземляра класса для валидации формы EditProfile
+const formValidatorEditProfile = new FormValidator(
+  parametrs,
+  parametrs.formSelector
+);
+formValidatorEditProfile.enableValidation();
 //Создание нового объекта - экземляра класса для валидации формы AddCard
-const formValidatorAddCard = new FormValidator(parametrs, parametrs.formSelectorAddCard)
-formValidatorAddCard.enableValidation()
+const formValidatorAddCard = new FormValidator(
+  parametrs,
+  parametrs.formSelectorAddCard
+);
+formValidatorAddCard.enableValidation();
 
-export {openImageCardHandler}
-export {parametrs}
+export { openImageCardHandler };
+export { parametrs };
